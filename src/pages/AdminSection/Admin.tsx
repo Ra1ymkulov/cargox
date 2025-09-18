fd623435a964b3d377c735a6361e86ca67c365e0
Ra1ymkulov <retype.uun@gmail.com>
2025-09-18 19:39:48 +0600

    /admin

--- src/pages/AdminSection/Admin.tsx
+++ src/pages/AdminSection/Admin.tsx
@@ -5,7 +5,7 @@ import scss from "./Admin.module.scss";
 interface IAdminProps {}
 
 const Admin = ({}) => {
-  return <div className={scss.admin}>Admin page</div>;
+  return <div className={scss.admin}>Admin page aza</div>;
 };
 
 export default Admin;
