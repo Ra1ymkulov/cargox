7332d9db6716115933580015c22e36ea008fd460
!skhak <nurlanjunushaliev76@icloud.com>
2025-09-19 20:57:32 +0600

    iskhak-header/footer/public

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,11 +1,32 @@
 "use client";
 import React from "react";
 import scss from "./Header.module.scss";
+import Link from "next/link";
 
 interface IHeaderProps {}
 
 const Header = ({}) => {
-  return <header className={scss.header}>header</header>;
+  return (
+    <header className={scss.header}>
+      <div className="container">
+        <div className={scss.content}>
+          <div className={scss.logo}>
+            <img src="/image 8.svg" alt="" />
+          </div>
+          <nav>
+            <Link href="/service">Услуги</Link>
+            <Link href="/">Грузоперевозки</Link>
+            <Link href="/">Калькулятор</Link>
+            <Link href="/">Отзывы</Link>
+            <Link href="/">Контакты</Link>
+          </nav>
+          <Link href="/auth/signup">
+            <button>Войти</button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
