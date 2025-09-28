67ede82cfccc4908c6f2cbd395680436566e6b62
Ra1ymkulov <retype.uun@gmail.com>
2025-09-28 19:21:27 +0600

    hh

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,11 @@
 "use client";
 import React from "react";
 import scss from "./Home.module.scss";
-import TariffSection from "@/components/tariffSection/TariffSection";
 
 interface IHomeProps {}
 
 const Home = ({}) => {
-  return (
-    <div className={scss.home}>
-      <TariffSection />
-    </div>
-  );
+  return <div className={scss.home}></div>;
 };
 
 export default Home;
