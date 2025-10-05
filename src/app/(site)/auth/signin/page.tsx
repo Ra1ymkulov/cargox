2afefbdd2caa18e6301453bfeb5ebfeedae739bb
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-05 13:51:09 +0600

    iskhak-login/register

--- src/app/(site)/auth/signin/page.tsx
+++
@@ -1,6 +0,0 @@
-import SignIn from "@/pages/AuthSection/signIn/SignIn";
-import React from "react";
-
-const page = () => <SignIn />;
-
-export default page;
---
+++ src/pages/auth/register/Register.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { useState } from "react";
+import scss from "./Register.module.scss";
+
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
+
+import Link from "next/link";
+
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [repeatShowPassword, setRepeatShowPassword] = useState(false);
+  return (
+    <div className={scss.register}>
+      <div className="container">
+        <div className={scss.content}>
+          <h1>Регистрация</h1>
+          <div className={scss.getInfo}>
+            <div className={scss.userData}>
+              <div className={scss.input}>
+                <p>Регистрация</p>
+                <input type="text" />
+              </div>
+              <div className={scss.input}>
+                <p>Фамилия</p>
+                <input type="text" />
+              </div>
+              <div className={scss.input}>
+                <p>Телефонный номер</p>
+                <input type="text" />
+              </div>
+              <div className={scss.input}>
+                <p>Имя пользователя</p>
+                <input type="text" />
+              </div>
+              <div className={scss.input}>
+                <p>Email</p>
+                <input type="text" />
+              </div>
+              <div className={scss.input}>
+                <p>Город</p>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={scss.input}>
+              <p>Придумайте пароль</p>
+              <input type={showPassword ? "password" : "text"} />
+              {showPassword ? (
+                <FiEyeOff
+                  className={scss.icon}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              ) : (
+                <FiEye
+                  className={scss.icon}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              )}
+            </div>
+            <div className={scss.input}>
+              <p>Повторите пароль</p>
+              <input type={repeatShowPassword ? "password" : "text"} />
+              {repeatShowPassword ? (
+                <FiEye
+                  className={scss.icon}
+                  onClick={() => setRepeatShowPassword(!repeatShowPassword)}
+                />
+              ) : (
+                <FiEyeOff
+                  className={scss.icon}
+                  onClick={() => setRepeatShowPassword(!repeatShowPassword)}
+                />
+              )}
+            </div>
+            <button>Зарегистрироваться</button>
+            <p>
+              Уже есть аккаунт? <Link href="/auth/login">Войти</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
