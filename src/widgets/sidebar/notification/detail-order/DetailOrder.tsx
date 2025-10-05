e2208220024aafab68b49e823eb50902aaed7343
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-05 12:39:13 +0600

    iskhak-detail/message

--- src/widgets/sidebar/notification/detail-order/DetailOrder.tsx
+++ src/widgets/sidebar/notification/detail-order/DetailOrder.tsx
@@ -3,8 +3,63 @@ import scss from "./DetailOrder.module.scss";
 const DetailOrder = () => {
   return (
     <div className={scss.detail}>
-      <img src="/logo-cargoX.svg" alt="" />
-      <p>detail</p>
+      <div className={scss.message}>
+        <div className={scss.top}>
+          <img src="/logo-cargoX.svg" alt="" />
+          <p>Ваш заказ доставлен !</p>
+        </div>
+        <div className={scss.main}>
+          <h1>
+            <i>
+              Трек-код: <span>#2d2ko3d93m92dd3qd</span>
+            </i>
+          </h1>
+          <div className={scss.list}>
+            <p>
+              Создано
+              <strong></strong>
+              <span>12:43</span>
+            </p>
+            <p>
+              Доставлено <strong></strong>
+              <span>12:45</span>
+            </p>
+            <p>
+              Время
+              <strong></strong>
+              <span>2мин</span>
+            </p>
+            <p>
+              Откуда
+              <strong></strong>
+              <span>Москва</span>
+            </p>
+            <p>
+              Куда
+              <strong></strong>
+              <span>Бишкек</span>
+            </p>
+            <p>
+              Группаперевозки
+              <strong></strong>
+              <span>Авиаперевозка</span>
+            </p>
+            <p>
+              Кому
+              <strong></strong>
+              <span>Baby John</span>
+            </p>
+            <p>
+              Где можете забрать
+              <strong></strong>
+              <span>
+                Улица Токтогула, 214/1  ​Ленинский район, Бишкек​720010
+              </span>
+            </p>
+          </div>
+          <img src="/adress-message.svg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
