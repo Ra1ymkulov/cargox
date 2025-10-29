40a7a27e54d7fa622a2f9ce93add131249b124bf
!skhak <nurlanjunushaliev76@icloud.com>
2025-10-29 20:50:37 +0600

    iskhak-status/homeBanner

--- src/features/status/api/statusApi.ts
+++ src/features/status/api/statusApi.ts
@@ -3,18 +3,18 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useChangeStatus = () => {
   const queryClient = useQueryClient();
+
   return useMutation<STATUS.GetStatusRes, Error, STATUS.GetStatusReq>({
     mutationFn: async ({ id, status, userId }) => {
-        console.log(id);
-      const response = await ORDER_API.post(
-        `/status-change/${id}/${status}`,
-        "c8ae0841-9d1b-486f-bcdc-382806470b7b"
-      );
+      const response = await ORDER_API.post(`/status-change/${id}`, {
+        status,
+        userId,
+      });
 
-      return response;
+      return response.data;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["status"] });
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
     },
   });
 };
