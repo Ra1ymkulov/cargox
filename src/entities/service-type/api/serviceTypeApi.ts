9581b15d875e8d085c63ee0081a4aeabf1dba60c
Ra1ymkulov <retype.uun@gmail.com>
2025-10-30 14:56:57 +0600

    dev

--- src/entities/service-type/api/serviceTypeApi.ts
+++ src/entities/service-type/api/serviceTypeApi.ts
@@ -6,7 +6,6 @@ const useGetServiceTypeQuery = () => {
     queryKey: [`/service-type`],
     queryFn: async () => {
       const response = await USER_API.get(`/get-service-type`);
-      const response = await ORDER_API.get(`/get-service-type`);
       return response.data.data;
     },
   });
