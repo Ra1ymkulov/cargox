31a403504a1fb78e51ea27af3bb70d72f0d1efd8
!skhak <nurlanjunushaliev76@icloud.com>
2025-11-02 15:10:01 +0600

    iskhak-header/adaptation

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,60 +1,113 @@
 "use client";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import React from "react";
-import scss from "./Header.module.scss";
-import { FiBell } from "react-icons/fi";
-import { FaRegUser } from "react-icons/fa";
 import Link from "next/link";
+import { FiBell, FiMenu } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+import scss from "./Header.module.scss";
 
 const Header = () => {
+  const router = useRouter();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   const items = [
     { name: "Тарифы", path: "/tariffs" },
     { name: "Отзывы", path: "/reviews" },
     { name: "Контакты", path: "/contact" },
   ];
-  const router = useRouter();
-  return (
-    <header className={scss.header}>
-      <div className="container">
-        <div className={scss.content}>
-          <div
-            onClick={() => {
-              router.push("/");
-            }}
-            className={scss.logo}
-          >
-            <img src="/logo-cargoX.svg" alt="logo project" />
-          </div>
 
-          <nav className={scss.nav}>
+  return (
+    <>
+      <header className={`${scss.header} ${isSidebarOpen ? scss.open : ""}`}>
+        <div className="container">
+          <div className={scss.content}>
+            <div className={scss.logo} onClick={() => router.push("/")}>
+              <img src="/logo-cargoX.svg" alt="Cargo X Logo" />
+            </div>
+
+            <nav className={scss.nav}>
+              <select className={scss.select}>
+                <option value="Услуги">Услуги</option>
+              </select>
+
+              {items.map((item) => (
+                <Link key={item.name} href={item.path} className={scss.item}>
+                  {item.name}
+                </Link>
+              ))}
+            </nav>
+
+            <div className={scss.user}>
+              <FiBell
+                fontSize={22}
+                color="white"
+                style={{ cursor: "pointer" }}
+                onClick={() => router.push("/user/notification")}
+              />
+              <FaRegUser
+                fontSize={20}
+                color="white"
+                style={{ cursor: "pointer" }}
+                onClick={() => router.push("/user")}
+              />
+              <div
+                className={scss.burgerMenu}
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              >
+                {!isSidebarOpen ? (
+                  <FiMenu fontSize={24} color="white" />
+                ) : (
+                  <IoMdClose fontSize={24} color="white" />
+                )}
+              </div>
+              <div
+                className={scss.burgerMenu}
+                onClick={() => setIsSidebarOpen(false)}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </header>
+      {isSidebarOpen && (
+        <aside className={scss.sidebar}>
+          <nav className={scss.sidebarNav}>
             <select className={scss.select}>
               <option value="Услуги">Услуги</option>
             </select>
 
             {items.map((item) => (
-              <Link key={item.name} href={item.path} className={`${scss.item}`}>
+              <Link
+                key={item.name}
+                href={item.path}
+                onClick={() => setIsSidebarOpen(false)}
+                className={scss.item}
+              >
                 {item.name}
               </Link>
             ))}
-          </nav>
 
-          <div className={scss.user}>
-            <FiBell
-              style={{ cursor: "pointer" }}
-              onClick={() => router.push("/user/notification")}
-              fontSize={24}
-              color="white"
-            />
-            <FaRegUser
-              style={{ cursor: "pointer" }}
-              onClick={() => router.push("/user")}
-              fontSize={22}
-              color="#fff"
-            />
-          </div>
-        </div>
-      </div>
-    </header>
+            <div className={scss.sidebarIcons}>
+              <FiBell
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setIsSidebarOpen(false);
+                  router.push("/user/notification");
+                }}
+              />
+              <FaRegUser
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setIsSidebarOpen(false);
+                  router.push("/user");
+                }}
+              />
+            </div>
+          </nav>
+        </aside>
+      )}
+      <div className={scss.overlay} onClick={() => setIsSidebarOpen(false)} />
+    </>
   );
 };
 
